26ebfe5dded3343535a0012063a55c165739255d
吃苹果的猫 <jiaoyaning1210@163.com>
2020-06-03 20:57:26 +0800

    updata 干巴爹

---
+++ custom_view/src/main/java/com/jyn/custom_view/views/Dashboard.java
@@ -0,0 +1,25 @@
+package com.jyn.custom_view.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by jiaoyaning on 2020/6/2.
+ */
+public class Dashboard extends View {
+
+    public Dashboard(Context context) {
+        super(context);
+    }
+
+    public Dashboard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Dashboard(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}

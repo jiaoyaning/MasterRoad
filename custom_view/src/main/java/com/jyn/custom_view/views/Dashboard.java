18cc2600ef5fa398e5e4a9ba0ce545159d6b6428
吃苹果的猫 <jiaoyaning1210@163.com>
2020-06-08 22:48:45 +0800

    update 添加custom

--- custom_view/src/main/java/com/jyn/custom_view/views/Dashboard.java
+++ custom_view/src/main/java/com/jyn/custom_view/views/Dashboard.java
@@ -1,6 +1,9 @@
 package com.jyn.custom_view.views;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Looper;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,16 +13,25 @@ import androidx.annotation.Nullable;
  * Created by jiaoyaning on 2020/6/2.
  */
 public class Dashboard extends View {
+    Paint mPaint;
 
     public Dashboard(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Dashboard(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public Dashboard(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);//抗锯齿
+        mPaint.setStyle(Paint.Style.STROKE);//画线模式
+//        mPaint.setStrokeWidth(Utils.px2dp(2));//线宽度
+        mPaint.setColor(Color.BLACK);
     }
 }

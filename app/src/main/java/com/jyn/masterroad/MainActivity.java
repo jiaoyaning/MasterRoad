23cd6cf1aa1821767069875be0f788f97e231636
jiaoyaning <jiaoyaning@igengmei.com>
2020-11-05 16:37:08 +0800

    update 终于成功

--- app/src/main/java/com/jyn/masterroad/MainActivity.java
+++ app/src/main/java/com/jyn/masterroad/MainActivity.java
@@ -1,32 +1,44 @@
-package com.jyn.masterroad
+package com.jyn.masterroad;
 
-import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.alibaba.android.arouter.facade.annotation.Route
-import com.alibaba.android.arouter.launcher.ARouter
-import com.jyn.masterroad.base.RoutePath
-import com.jyn.masterroad.databinding.ActivityMainBinding
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.jyn.masterroad.base.BaseActivity;
+import com.jyn.masterroad.databinding.ActivityMainBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import static com.jyn.masterroad.base.RoutePath.HANDLER;
+import static com.jyn.masterroad.base.RoutePath.MAIN;
 
 /**
  * https://www.jianshu.com/p/2ee3672efb1f
  */
-@Route(path = RoutePath.MAIN)
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var activityMainBinding: ActivityMainBinding
+@Route(path = MAIN)
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @NotNull
+    private ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(activityMainBinding.root);
-//        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.main_layout)
-        //        activityMainBinding.handlerTest.setOnClickListener(this);
+        setContentView(activityMainBinding.getRoot());
+        activityMainBinding.setMainViewModel(new MainViewModel());
+//        activityMainBinding.handlerTest.setOnClickListener(this);
     }
 
-    override fun onClick(v: View) {
-        if (v.id == R.id.handler_test) {
-            ARouter.getInstance().build(RoutePath.HANDLER).navigation()
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.handler_test) {
+            ARouter.getInstance().build(HANDLER).navigation();
         }
     }
 }
--- app/src/main/java/com/jyn/masterroad/MainViewModel.kt
+++ app/src/main/java/com/jyn/masterroad/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.jyn.masterroad
 import android.view.View
 import androidx.lifecycle.ViewModel
 import com.alibaba.android.arouter.launcher.ARouter
+import com.jyn.masterroad.base.RoutePath
 
 /**
  * Created by jiaoyaning on 2020/11/2.
@@ -11,6 +12,6 @@ class MainViewModel : ViewModel() {
     val handler = "Handler"
 
     fun goToHandler(view: View) {
-        ARouter.getInstance().build(handler).navigation();
+        ARouter.getInstance().build(RoutePath.HANDLER).navigation();
     }
 }

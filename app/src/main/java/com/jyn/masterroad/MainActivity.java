55f9b40d0977ee76132bdcdbb5809706b4f70780
jiaoyaning <jiaoyaning@igengmei.com>
2020-11-05 16:29:42 +0800

    好歹也算是实现了双向绑定

--- app/src/main/java/com/jyn/masterroad/MainActivity.java
+++ app/src/main/java/com/jyn/masterroad/MainActivity.java
@@ -1,40 +1,32 @@
-package com.jyn.masterroad;
+package com.jyn.masterroad
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
-import com.jyn.masterroad.base.BaseActivity;
-import com.jyn.masterroad.databinding.ActivityMainBinding;
-
-import static com.jyn.masterroad.base.RoutePath.HANDLER;
-import static com.jyn.masterroad.base.RoutePath.MAIN;
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
+import com.jyn.masterroad.base.RoutePath
+import com.jyn.masterroad.databinding.ActivityMainBinding
 
 /**
  * https://www.jianshu.com/p/2ee3672efb1f
  */
-@Route(path = MAIN)
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+@Route(path = RoutePath.MAIN)
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var activityMainBinding: ActivityMainBinding
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(activityMainBinding.getRoot());
-//        activityMainBinding.handlerTest.setOnClickListener(this);
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.main_layout);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.root);
+//        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.main_layout)
+        //        activityMainBinding.handlerTest.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.handler_test) {
-            ARouter.getInstance().build(HANDLER).navigation();
+    override fun onClick(v: View) {
+        if (v.id == R.id.handler_test) {
+            ARouter.getInstance().build(RoutePath.HANDLER).navigation()
         }
     }
 }

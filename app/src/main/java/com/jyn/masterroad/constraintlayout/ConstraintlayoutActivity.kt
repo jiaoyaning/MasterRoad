72acee4284910b85f4baa7d4b39db8685817f577
jiaoyaning <jiaoyaning@igengmei.com>
2020-12-02 18:47:52 +0800

    update

---
+++ app/src/main/java/com/jyn/masterroad/constraintlayout/ConstraintlayoutActivity.kt
@@ -0,0 +1,17 @@
+package com.jyn.masterroad.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jyn.masterroad.R
+
+/**
+ * https://juejin.cn/post/6854573221312725000
+ *
+ * http://www.flyou.ren/2019/10/10/ConstraintLayout/
+ */
+class ConstraintlayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraintlayout)
+    }
+}
---
+++ app/src/main/java/com/jyn/masterroad/constraintlayout/MotionLayoutActivity.kt
@@ -0,0 +1,15 @@
+package com.jyn.masterroad.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jyn.masterroad.R
+
+/**
+ * https://juejin.cn/post/6854573206653812743
+ */
+class MotionLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_layout)
+    }
+}

ef640e3b9b0c986d6c094a02f426453938a7459b
吃苹果的猫 <jiaoyaning1210@163.com>
2020-12-09 22:47:49 +0800

    update kotlin 函数类型

--- java_kotlin/src/main/java/com/jyn/java_kotlin/kotlinlearn/Learn.kt
+++ java_kotlin/src/main/java/com/jyn/java_kotlin/kotlinlearn/Learn.kt
@@ -3,14 +3,23 @@ package com.jyn.java_kotlin.kotlinlearn
 /**
  * Created by jiaoyaning on 2020/12/8.
  */
-class Learn {
-
-}
-
 fun main() {
+    println("============扩展特性=============")
     10.sayHollow()
     10.testAttr = " 猜猜我是几？ "
     println(10.testAttr)
+
+    println("============方法类型=============")
+    functionTypeTest1 { println("这是一个无参无返回值的函数类型 ") }//大括号表示这是一个方法
+    functionTypeTest1(Learn()::fun1) //方法调用，要用小括号
+
+    //TODO 有参无返回值的函数类型 失败
+    functionTypeTest2 { println("这是一个有参无返回值的内部函数类型 int -> $it") }
+    functionTypeTest2(::intFun)
+}
+
+fun intFun(int: Int) {
+    println("这是一个有参无返回值的外部函数类型 int -> $int")
 }
 
 /**
@@ -28,3 +37,18 @@ var Int.testAttr: String
     set(value) {
         println("我设置了一个成员属性${value}")
     }
+
+//函数类型
+fun functionTypeTest1(fun1: () -> Unit) {
+    fun1()
+}
+
+fun functionTypeTest2(fun1: (intTest: (Int)) -> Unit) {
+    fun1(10)
+}
+
+class Learn {
+    fun fun1() {
+        println("这是一个无参无返回值的方法")
+    }
+}

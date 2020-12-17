8933640ca47a343a4a0bd9586daf162b06191185
jiaoyaning <jiaoyaning@igengmei.com>
2020-12-17 17:56:57 +0800

    update Java9 接口可以实现私有方法

--- java_kotlin/src/main/java/com/jyn/java_kotlin/javatest/DefaultFunInterfaceTest.java
+++ java_kotlin/src/main/java/com/jyn/java_kotlin/javatest/DefaultFunInterfaceTest.java
@@ -1,12 +1,19 @@
 package com.jyn.java_kotlin.javatest;
 
 /**
- * Java8 接口方法测试
+ * Java8 Java9 接口方法测试
+ *
+ * Java8 中，接口可以实现如下变量/方法
+ * 常量
+ * 抽象方法
+ * 默认方法
+ * 静态方法
  */
 public class DefaultFunInterfaceTest implements DefaultFunInterface {
 
     public static void main(String[] args) {
         DefaultFunInterfaceTest test = new DefaultFunInterfaceTest();
+        System.out.println(variable);
 
         System.out.println(test.notDefaultTest());
         System.out.println(test.defaultTest());
@@ -16,5 +23,13 @@ public class DefaultFunInterfaceTest implements DefaultFunInterface {
     @Override
     public String notDefaultTest() {
         return "接口方法重写后的返回值";
+    }
+
+    /**
+     * 可重写可不写
+     */
+    @Override
+    public String defaultTest() {
+        return null;
     }
 }

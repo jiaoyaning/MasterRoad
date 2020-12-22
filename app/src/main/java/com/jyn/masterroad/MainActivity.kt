ca53b97777e99e0d5c44334e94e7357e520bf128
jiaoyaning <jiaoyaning@igengmei.com>
2020-12-22 19:44:29 +0800

    update

--- app/src/main/java/com/jyn/masterroad/MainActivity.kt
+++ app/src/main/java/com/jyn/masterroad/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jyn.masterroad.databinding.ActivityMainBinding
 
+/**
+ * Messenger与AIDL的异同:
+ * https://blog.csdn.net/jiwangkailai02/article/details/48098087
+ */
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {

641f227edc96d700487be93566bde1065f92c686
吃苹果的猫 <jiaoyaning1210@163.com>
2020-12-22 22:27:35 +0800

    update motion_layout

--- app/src/main/java/com/jyn/masterroad/constraintlayout/MotionLayoutActivity.kt
+++ app/src/main/java/com/jyn/masterroad/constraintlayout/MotionLayoutActivity.kt
@@ -8,6 +8,12 @@ import com.jyn.masterroad.base.RoutePath
 import kotlinx.android.synthetic.main.activity_motion_layout.*
 
 /**
+ * 官方文档：
+ * https://developer.android.google.cn/training/constraint-layout/motionlayout?hl=zh_cn
+ *
+ * 官方实例：
+ * https://developer.android.google.cn/training/constraint-layout/motionlayout/examples?hl=zh_cn
+ *
  * https://juejin.cn/post/6854573206653812743
  *
  * https://juejin.cn/post/6844903918598635534

93d524707896c070d4769b78087e4414111b4fcd
吃苹果的猫 <jiaoyaning1210@163.com>
2021-01-17 11:42:58 +0800

    update 合并plugin

--- Plugin/PluginHelloWorld/README.md
+++ Plugin/PluginHelloWorld/README.md
@@ -4,4 +4,6 @@ https://www.cnblogs.com/huansky/p/12942309.html
 本地插件使用(视频)
 https://www.bilibili.com/video/BV1yz4y1C73H?p=5
 
+https://juejin.cn/post/6844903752969748493
+
 https://blog.csdn.net/wise18146705004/article/details/104203205
--- Plugin/PluginHelloWorld/src/main/java/com.jyn.plugin/HelloWorld.java
+++ Plugin/PluginHelloWorld/src/main/java/com.jyn.plugin/HelloWorld.java
@@ -3,10 +3,10 @@ package com.jyn.plugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
-class GreetingPlugin implements Plugin<Project> {
+class HelloWorld implements Plugin<Project> {
 
     @Override
     public void apply(Project target) {
-        System.out.println("----- 加载插件 ------" + target.getName());
+        System.out.println("----- 加载插件 ------");
     }
 }

1cc7c1e0c6046ed6848d46eed8e5b527f3c4290b
jiaoyaning <jiaoyaning@igengmei.com>
2021-02-20 18:05:15 +0800

    update 线程

---
+++ app/src/main/java/com/jyn/masterroad/Thread/ExecutorsTest.kt
@@ -0,0 +1,64 @@
+package com.jyn.masterroad.Thread
+
+import com.apkfuns.logutils.LogUtils
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+class ExecutorsTest {
+
+    private val executor: ExecutorService by lazy {
+//        Executors.newCachedThreadPool()           //创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
+//        Executors.newSingleThreadExecutor()       //创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
+//        Executors.newScheduledThreadPool(3)       //创建一个定长线程池，支持定时及周期性任务执行、延迟执行
+        Executors.newFixedThreadPool(2)     //创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待。
+    }
+
+    private var threadName = 1
+    fun startExecutors() {
+        executor.submit {
+            val position = threadName++
+            for (i in 1..10) {
+                LogUtils.tag("main").i("Executors 创建的线程 线程名:$position 次数:$i")
+                Thread.sleep(1000)
+            }
+        }
+    }
+
+    // region 一、线程池关闭
+    /*
+     * 1.将线程池的状态设置为 SHUTDOWN
+     * 2.不会立即停止线程池，会继续执行尚未结束的任务 & 任务队列中的任务，直到全部执行完毕
+     * 3.不再接受新的任务，强行添加会报错(submit 和 execute 方式都一样) --RejectedExecutionException
+     */
+    fun shutdown() {
+        executor.shutdown()
+    }
+
+    /*
+     * 1.将线程池的状态设置为 STOP
+     * 2.正在执行的任务会被尝试 interrupt()中断，没被执行的任务则被返回，不包含正在执行的任务。
+     */
+    fun shutdownNow() {
+        val shutdownNow = executor.shutdownNow()
+        LogUtils.tag("main").i("Executors.shutdownNow() 还未被执行的线程数量:${shutdownNow.size}")
+    }
+
+    /*
+     * 主线程等各子线程都运行完毕后再执行，可应用于设置多少秒后强制销毁线程池
+     * 第一个参数指定的是时间，第二个参数指定的是时间单位(当前是秒)
+     *
+     * 1.awaitTermination()方法所在线程会陷入阻塞状态，阻塞时间就是所设置时间（阻塞时间可以设置超过线程池本身所需的执行时间）
+     *   阻塞地点就是awaitTermination()方法。
+     * 2.线程池行为和shutdown()相同，只是阻塞时间内不可继续添加线程（强行添加不会抛异常），过了阻塞期后可以继续添加
+     * 3.返回结果 = 阻塞结束时线程池是否已被销毁 (shutdown和shutdownNow方法能销毁线程池)
+     */
+    fun awaitTermination() {
+        LogUtils.tag("main").i("Executors.awaitTermination() 开始等待……")
+        executor.shutdown() //shutdown之后线程池才能被termination。
+        val awaitTermination = executor.awaitTermination(3, TimeUnit.SECONDS)
+        LogUtils.tag("main").i("Executors.awaitTermination() 等待结束！！")
+        LogUtils.tag("main").i("Executors.awaitTermination() 返回结果:${awaitTermination}")
+    }
+    // endregion
+}
--- app/src/main/java/com/jyn/masterroad/Thread/ThreadCreate.kt
+++ app/src/main/java/com/jyn/masterroad/Thread/ThreadCreate.kt
@@ -2,10 +2,11 @@ package com.jyn.masterroad.Thread
 
 import com.apkfuns.logutils.LogUtils
 import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.FutureTask
 
 class ThreadCreate {
 
-    // 1.继承Thread，重写run()方法
     class ThreadTest : Thread() {
         override fun run() {
             super.run()
@@ -13,18 +14,62 @@ class ThreadCreate {
         }
     }
 
-    // 2.实现Runnable接口，重写run()方法。可避免Java单继承导致的无法继承Thread实现线程，然而依然需要Thread类才可创建线程
     class RunnableTest : Runnable {
+        @Volatile
+        var countDown = 4
+
+        @Synchronized
         override fun run() {
-            LogUtils.tag("main").i("实现 Runnable 后创建的线程")
+            while (countDown-- > 0) {
+                LogUtils.tag("main").i("实现 Runnable 后创建的线程 countDown:$countDown")
+            }
         }
     }
 
-    // 3.使用FutureTask类和Callable接口
     class CallableTest : Callable<String> {
         override fun call(): String {
             LogUtils.tag("main").i("实现 Callable 后创建的线程，可以携带返回值")
             return "这是一个实现了Callable接口的线程"
         }
     }
+
+    // region 一、线程创建的四种方式
+    // 1.继承Thread类
+    fun threadTest() {
+        val threadTest = ThreadTest()
+        threadTest.start()
+    }
+
+    // 2.实现Runnable接口，可避免Java单继承问题，仍需要Thread类
+    fun runnableThreadTest() {
+        val runnableTest = RunnableTest()
+        // 多线程情况下可共享变量，但并不能保证线程安全，只加volatile不行，同时需要Synchronized
+        val runnableThreadTest1 = Thread(runnableTest)
+        val runnableThreadTest2 = Thread(runnableTest)
+        runnableThreadTest1.start()
+        runnableThreadTest2.start()
+    }
+
+    // 3.FutureTask类 + Callable接口，可携带返回值，需要Thread类或者Executors
+    fun futureTaskTest() {
+        val callableTest: Callable<String> = ThreadCreate.CallableTest()
+        val futureTaskTest: FutureTask<String> = FutureTask<String>(callableTest)
+
+        //第一种futureTask方式 使用线程池
+        val executor = Executors.newCachedThreadPool()
+        val submit = executor.submit(callableTest)
+        LogUtils.tag("main").i("第一种Future实现方式: Executors.submit(Callable) 返回结果:${submit.get()}")
+        executor.shutdown()
+
+        //第二种futureTask方式 使用Thread
+        Thread(futureTaskTest).start()
+        LogUtils.tag("main").i("第二种Future实现方式: Thread(FutureTask) 返回结果:${futureTaskTest.get()}")
+    }
+
+    // 4.借助Executors(线程池)
+    fun executorsTest() {
+        val es = Executors.newCachedThreadPool()
+        es.execute { LogUtils.tag("main").i("使用 Executors 创建的线程") }
+    }
+    // endregion
 }

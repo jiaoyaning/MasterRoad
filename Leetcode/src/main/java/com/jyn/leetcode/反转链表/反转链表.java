a42600b5e1099ba3f29cdc02c010488a720eba37
jiaoyaning <jiaoyaning@igengmei.com>
2021-05-12 11:20:16 +0800

    update 反转链表

--- Leetcode/src/main/java/com/jyn/leetcode/反转链表/反转链表.java
+++ Leetcode/src/main/java/com/jyn/leetcode/反转链表/反转链表.java
@@ -1,12 +1,12 @@
-package com.jyn.leetcode.翻转链表;
+package com.jyn.leetcode.反转链表;
 
 import com.jyn.leetcode.ListNode;
 
 /**
- * Created by jiao on 2020/9/1.
  * 反转链表
+ * https://labuladong.gitbook.io/algo/mu-lu-ye-1/mu-lu-ye/di-gui-fan-zhuan-lian-biao-de-yi-bu-fen
  */
-public class 翻转链表 {
+public class 反转链表 {
     public static void main(String[] args) {
         ListNode.print(ListNode.getListNodeTest());
         ListNode listNode = reverseList(ListNode.getListNodeTest());
@@ -31,29 +31,23 @@ public class 翻转链表 {
     //region 一、递归反转整个链表
     /*
      * 原始链表： 1 -> 2 -> 3 -> null
-     *
      * 第一遍
      * ①: last = 3 ; head = 2
-     *
      * ②: 1 -> 2 <-> 3
-     *
      * ③: 1 -> 2 <- 3
      *          ↓
      *         null
-     *
      * 第二遍
      * ①: last = 3 ; head = 1
-     *
      * ②: 1 <-> 2 <- 3
-     *
      * ③: 1 <- 2 <- 3
      *     ↓
      *    null
      */
     public static ListNode reverseList2(ListNode head) {
         if (head.next == null)
-            return head; //递归的终点
-        ListNode last = reverseList2(head.next); //① 递归的终点会一直传递到第一个递归点，并没有被改变
+            return head; //终点head
+        ListNode last = reverseList2(head.next); //① 终点head会一直传递到第一个递归点，所以返回值一直都是终点head
         head.next.next = head;  //②
         head.next = null;       //③
         return last;
@@ -79,6 +73,15 @@ public class 翻转链表 {
         head.next = successor;
         return last;
     }
+    //endregion
 
+    //region 三、反转链表的一部分
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+        if (n == 1) { //N 等于 1 的时候，相当于反转前 n 个元素
+            return reverseN(head, n);
+        }
+        head.next = reverseBetween(head, m - 1, n - 1); //找出需要反转的位置，并把箭头指向它的反转结果
+        return head; //始终是返回head
+    }
     //endregion
 }

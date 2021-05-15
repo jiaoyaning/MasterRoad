bea577f2f1513d1f205897daa4a1a25c7e12d01e
jiaoyaning <jiaoyaning@igengmei.com>
2021-05-15 19:59:48 +0800

    update koin

--- app/src/main/java/com/jyn/masterroad/kotlin/koin/data/KoinViewModel.kt
+++ app/src/main/java/com/jyn/masterroad/kotlin/koin/data/KoinViewModel.kt
@@ -3,16 +3,25 @@ package com.jyn.masterroad.kotlin.koin.data
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.apkfuns.logutils.LogUtils
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import org.koin.core.context.GlobalContext
 
-class KoinViewModel(var repo: HelloRepository) : ViewModel() {
+/*
+ * 当我们其他类中使用时需要实现 KoinComponent 接口或者直接 GlobalContext 类获取
+ */
+class KoinViewModel(var repo: HelloRepository) : ViewModel(), KoinComponent {
     companion object {
         const val TAG = "Koin"
     }
 
+    private val parameterData1: ParameterData = get()
+    private val parameterData2: ParameterData = GlobalContext.get().get()
+
     var num: MutableLiveData<Int> = MutableLiveData(0)
 
     init {
-        LogUtils.tag(TAG).i("KoinTestData ——> 初始化")
+        LogUtils.tag(TAG).i("KoinTestData ——> 初始化 $parameterData1 ; $parameterData2")
     }
 
     fun add() {

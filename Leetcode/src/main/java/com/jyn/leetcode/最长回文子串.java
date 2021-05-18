0d5e5e2ae47d91eb7ff2a24102c441a895280320
吃苹果的猫 <jiaoyaning1210@163.com>
2021-05-18 22:52:43 +0800

    最长回文子串

---
+++ Leetcode/src/main/java/com/jyn/leetcode/最长回文子串.java
@@ -0,0 +1,34 @@
+package com.jyn.leetcode;
+
+/*
+ * 如何寻找最长回文子串
+ * https://labuladong.gitee.io/algo/5/39/
+ */
+class 最长回文子串 {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babbab"));
+    }
+
+    public static String longestPalindrome(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i + 1);
+            result = result.length() > s1.length() ? result : s1;
+            result = result.length() > s2.length() ? result : s2;
+        }
+        return result;
+    }
+
+    /*
+     * 寻找回文字串
+     */
+    public static String palindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() &&
+                s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+}
---
+++ experience/README.md
@@ -0,0 +1,2 @@
+## 两年Android开发大厂面试经验
+https://mp.weixin.qq.com/s/kyG2sfhuVFHB85SQuxF_VQ

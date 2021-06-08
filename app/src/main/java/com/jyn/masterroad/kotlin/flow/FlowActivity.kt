2af1bdb15bc210fa61916d2fe018a923b7d92032
jiaoyaning <jiaoyaning@igengmei.com>
2021-06-08 17:33:44 +0800

    update flow

--- app/src/main/java/com/jyn/masterroad/kotlin/flow/FlowActivity.kt
+++ app/src/main/java/com/jyn/masterroad/kotlin/flow/FlowActivity.kt
@@ -10,6 +10,9 @@ import com.jyn.masterroad.databinding.ActivityFlowBinding
  *
  * Android 上的 Kotlin 数据流
  * https://developer.android.com/kotlin/flow?hl=zh-cn
+ *
+ * Kotlin Flow 场景化学习
+ * https://mp.weixin.qq.com/s/XfJ4Gkp_9Ww5Dxx8Di_AVg
  */
 class FlowActivity : BaseActivity<ActivityFlowBinding>(R.layout.activity_flow) {
 

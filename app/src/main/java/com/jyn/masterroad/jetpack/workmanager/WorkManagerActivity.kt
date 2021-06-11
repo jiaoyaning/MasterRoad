bc8cbac1cd010d2222ff1fa0c48665044af498ec
jiaoyaning <jiaoyaning@igengmei.com>
2021-06-11 19:01:18 +0800

    update rxjava

--- app/src/main/java/com/jyn/masterroad/jetpack/workmanager/WorkManagerActivity.kt
+++ app/src/main/java/com/jyn/masterroad/jetpack/workmanager/WorkManagerActivity.kt
@@ -5,6 +5,12 @@ import com.jyn.masterroad.base.BaseActivity
 import com.jyn.masterroad.databinding.ActivityWorkManagerBinding
 
 /*
+ * Jetpack架构组件篇—WorkManager使用篇
+ * https://mp.weixin.qq.com/s/Z_LF_QuXPljaq5-AYDWvXg
+ *
+ * WorkManager 流程分析和源码解析 | 开发者说·DTalk
+ * https://mp.weixin.qq.com/s/MPZD9LbSbJYx1BLAolIbIg
+ *
  * 使用 WorkManager 处理需要立刻执行的后台任务
  * https://mp.weixin.qq.com/s/rTyBHtULXbCNjBdVuE-VTA
  */
--- app/src/main/java/com/jyn/masterroad/utils/rxjava/RxJavaCreate.kt
+++ app/src/main/java/com/jyn/masterroad/utils/rxjava/RxJavaCreate.kt
@@ -1,29 +1,38 @@
 package com.jyn.masterroad.utils.rxjava
 
 import com.apkfuns.logutils.LogUtils
-import io.reactivex.Observable
-import io.reactivex.ObservableOnSubscribe
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Consumer
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.Consumer
 import java.lang.Thread.sleep
 
+/**
+ * 五种观察者模式的创建
+ * https://blog.csdn.net/IT_fighter/article/details/103777107
+ *
+ *
+ */
 class RxJavaCreate {
     companion object {
         private const val TAG = "RxJava"
     }
 
-    /**
-     * 完全体
+//region 1. Observable + Observer/Consumer  发射数据
+    /*
+     * Observable: 能够发射0或n个数据，并以成功或错误事件终止
      */
+
+    //region 1.1 观察者 Observable.subscribe(observer)
     fun observerSimple() {
-        //region 1.Observable()  2.Observer()  3.observable.subscribe(observer)
         //1. 创建被观察者(Observable)，定义要发送的事件
         val observable = Observable.just(
-                "第 1 条数据",
-                "第 2 条数据",
-                "第 3 条数据",
-                "第 4 条数据")
+            "第 1 条数据",
+            "第 2 条数据",
+            "第 3 条数据",
+            "第 4 条数据"
+        )
 
         //2. 创建观察者(Observer)，接受事件并做出响应操作
         val observer: Observer<String> = object : Observer<String> {
@@ -46,13 +55,10 @@ class RxJavaCreate {
 
         //3. 观察者通过订阅（subscribe）被观察者把它们连接到一起。
         observable.subscribe(observer)
-        //endregion
-
-        //region
-
-        //endregion
     }
+    //endregion
 
+    //region 1.2. 消费者 Observable.subscribe(consumer)
     fun consumerSimple() {
         val observable = Observable.create(ObservableOnSubscribe<String> {
             LogUtils.tag(TAG).i("onNext Thread:${Thread.currentThread().name}")
@@ -76,9 +82,43 @@ class RxJavaCreate {
         sleep(500)
         LogUtils.tag(TAG).i("consumer sleep后 disposable: ${disposable.isDisposed}")
     }
+    //endregion
+//endregion
+
+//================================================
+
+//region 2. Flowable + Subscriber   背压
+    /*
+     * 能够发射0或n个数据，并以成功或错误事件终止。 支持背压(下游处理速度比上游发送速度要慢)，可以控制数据源发射的速度。
+     */
 
 
-    fun chainedSimple() {
+//endregion
 
-    }
+//================================================
+
+//region 3. Single + SingleObserver     单次发送：onSuccess / onError
+    /*
+     * 只发射单个数据或错误事件，如果发射器发射多条数据，观察者只能接收到第一条数据。
+     */
+
+//endregion
+
+//================================================
+
+//region 4. Completable + CompletableObserver   单次发送：onComplete / onError
+    /**
+     * 不发射数据，只处理 onComplete 和 onError 事件。方法onComplete与onError只可调用一个，同时调用，第一个生效。
+     */
+
+//endregion
+
+//================================================
+
+//region 5. Maybe + MaybeObserver   单次发送：onSuccess / onComplete / onError
+    /**
+     * 能够发射0或者1个数据，要么成功，要么失败。有点类似于Optional。
+     */
+
+//endregion
 }

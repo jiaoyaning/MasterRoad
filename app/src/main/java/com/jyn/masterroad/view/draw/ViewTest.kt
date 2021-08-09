cc4135623c109797f7e1fcc7af52a878025eb0f0
吃苹果的猫 <jiaoyaning1210@163.com>
2021-08-09 23:23:44 +0800

    update

--- app/src/main/java/com/jyn/masterroad/view/draw/ViewTest.kt
+++ app/src/main/java/com/jyn/masterroad/view/draw/ViewTest.kt
@@ -1,6 +1,8 @@
 package com.jyn.masterroad.view.draw
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -11,4 +13,18 @@ import android.view.View
 class ViewTest @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)//抗锯齿
+
+    override fun onDraw(canvas: Canvas) {
+        drawLine(canvas)
+        drawCircle(canvas)
+    }
+
+    private fun drawLine(canvas: Canvas) {
+        canvas.drawLine(50f, 50f, 200f, 50f, paint)
+    }
+
+    private fun drawCircle(canvas: Canvas) {
+        canvas.drawCircle(200f, 200f, 100f, paint)
+    }
 }

f80d732e585144b0bc40511cbef2c925a70e825a
吃苹果的猫 <jiaoyaning1210@163.com>
2021-08-09 23:11:06 +0800

    update 自定义view

---
+++ app/src/main/java/com/jyn/masterroad/view/draw/ViewTest.kt
@@ -0,0 +1,14 @@
+package com.jyn.masterroad.view.draw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+
+/**
+ * Created by jiaoyaning on 2021/8/9.
+ */
+class ViewTest @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+}

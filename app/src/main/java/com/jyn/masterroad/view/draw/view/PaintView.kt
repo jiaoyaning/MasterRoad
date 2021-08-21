6f643e22886d935e7f1bea7f2d7faf6d609ce9e0
吃苹果的猫 <jiaoyaning1210@163.com>
2021-08-21 13:00:56 +0800

    update shader

--- app/src/main/java/com/jyn/masterroad/view/draw/view/PaintView.kt
+++ app/src/main/java/com/jyn/masterroad/view/draw/view/PaintView.kt
@@ -52,12 +52,12 @@ class PaintView @JvmOverloads constructor(
          *      REPEAT 重复使用镜像模式的图像来填充多余空间
          */
         val linearGradient = LinearGradient(
-            100f, 0f, 200f, 300f,
+            0f, 0f, 200f, 200f,
             Color.parseColor("#00FFFF"), Color.parseColor("#FF00FF"),
             Shader.TileMode.CLAMP
         )
         paint.shader = linearGradient
-        canvas.drawRect(100f, 0f, 300f, 200f, paint)
+        canvas.drawRect(0f, 0f, 200f, 200f, paint)
         paint.reset()
 
         /**
@@ -70,12 +70,12 @@ class PaintView @JvmOverloads constructor(
          * tileMode     辐射范围之外的着色模式。
          */
         val radialGradient = RadialGradient(
-            500f, 100f, 100f,
+            350f, 100f, 100f,
             Color.parseColor("#00FFFF"), Color.parseColor("#FF00FF"),
             Shader.TileMode.MIRROR //这里需要把半径调小才能看出效果
         )
         paint.shader = radialGradient
-        canvas.drawCircle(500f, 100f, 100f, paint)
+        canvas.drawCircle(350f, 100f, 100f, paint)
         paint.reset()
 
         /**
@@ -85,11 +85,11 @@ class PaintView @JvmOverloads constructor(
          * color0 color1 两个端点分别的颜色值
          */
         val sweepGradient = SweepGradient(
-            800f, 100f,
+            600f, 100f,
             Color.parseColor("#00FFFF"), Color.parseColor("#FF00FF")
         )
         paint.shader = sweepGradient
-        canvas.drawCircle(800f, 100f, 100f, paint)
+        canvas.drawCircle(600f, 100f, 100f, paint)
         paint.reset()
 
         /**
@@ -98,13 +98,22 @@ class PaintView @JvmOverloads constructor(
          * bitmap：用来做模板的 Bitmap 对象
          * tileX：横向的 TileMode
          * tileY：纵向的 TileMode
+         *
+         * BitmapShader的绘制原理是从视图原点开始绘制第一个Bitmap,然后在按设置的模式先绘制Y轴，然后根据Y轴的绘制，绘制X轴这样去绘制超过Bitmap的部分。
+         * 按这样的原理绘制的图形此时并不会显示，只有当canvas调用draw绘制后，才显示canvas绘制的那部分。
          */
         val bitmap = getBitmap(R.mipmap.icon_master_road2)
 //        val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.icon_master_road2)
         val bitmapShader = BitmapShader(bitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT)
         paint.shader = bitmapShader
-        canvas.drawRect(100f, 250f, 300f, 450f, paint)
+        canvas.drawRect(750f, 0f, 950f, 200f, paint)
         paint.reset()
+
+
+        /**
+         * ComposeShader 混合着色器
+         * 所谓混合，就是把两个 Shader 一起使用
+         */
     }
 
     // 2. 效果
@@ -134,8 +143,6 @@ class PaintView @JvmOverloads constructor(
         options.inJustDecodeBounds = false //根据尺寸读取原图
         options.inDensity = options.outWidth
         options.inTargetDensity = 200
-        options.outHeight = 200
-        options.outWidth = 200
         return BitmapFactory.decodeResource(resources, id, options)
     }
 }

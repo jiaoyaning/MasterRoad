aa7868730d925e43bba4bc6f7dd548411f9deff8
jiaoyaning <jiaoyaning@igengmei.com>
2021-09-24 10:16:07 +0800

    observable 到 livedata 再到 flow

--- app/src/main/java/com/jyn/masterroad/jetpack/livedata/LiveDataTestVM.kt
+++ app/src/main/java/com/jyn/masterroad/jetpack/livedata/LiveDataTestVM.kt
@@ -11,27 +11,37 @@ import com.jyn.common.Base.BaseVM
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.Disposable
+import com.jyn.masterroad.kotlin.flow.FlowCreate
 
-/*
+/**
  * https://github.com/android/architecture-components-samples/tree/main/LiveDataSample
  *
  * 面试官：你了解 LiveData 的 postValue 吗？
  * https://mp.weixin.qq.com/s/yMO1oDSUAGmPJ4w4hqSWPw
+ *
+ * 再谈协程之viewmodel-livedata难兄难弟
+ * https://mp.weixin.qq.com/s/c36i8sTDhacHIkH8jGbMSg
+ *
+ * LiveData升级版 [FlowCreate]
  */
 class LiveDataTestVM(application: Application) : BaseVM(application) {
+    /**
+     * Observable -> LiveData
+     *   1. Observable 只有在数据发生改变时UI才会收到通知，
+     *      而 LiveData 不同，只要你 postValue 或者 setValue ，UI 都会收到通知，不管数据有无变化
+     *   2. Observable 无法感知生命周期，如果页面已销毁，可能会导致应用崩溃
+     *      而 LiveData 能感知 Activity 的生命周期，在 Activity 不活动的时候不会触发(位于start状态之后)，例如一个Activity不在任务栈顶部
+     *
+     * LiveData -> flow
+     *   1. LiveData 在连续 postValue 时，会导致数据丢失，只能收到最后有一条数据
+     *   2. LiveData 不具备数据处理能力比如 Rxjava 的各种操作符，其只保留了最简单的map、switchMap等有限的操作符
+     *   3. LiveData 粘性事件问题，LiveData在注册时，会触发LifecycleOwner的activeStateChanged，触发了onChange，导致在注册前的事件也被发送出来
+     */
 
     companion object {
         const val TAG = "LiveData"
     }
 
-    /**
-     * Observable只有在数据发生改变时UI才会收到通知，而LiveData不同，
-     * 只要你postValue或者setValue，UI都会收到通知，不管数据有无变化
-     *
-     * LiveData能感知Activity的生命周期，在Activity不活动的时候不会触发(位于start状态之后)，例如一个Activity不在任务栈顶部
-     *
-     * ps:Observable无法感知生命周期，如果页面已销毁，可能会导致应用崩溃
-     */
     var numString: MutableLiveData<String>? = null
         get() {
             if (field == null) {
@@ -66,15 +76,16 @@ class LiveDataTestVM(application: Application) : BaseVM(application) {
             super.onInactive()
         }
     }
+
     //region Observable
     private var numObservable = ObservableInt(0)
 
     init {
         numObservable.addOnPropertyChangedCallback(object :
-            androidx.databinding.Observable.OnPropertyChangedCallback() {
+                androidx.databinding.Observable.OnPropertyChangedCallback() {
             override fun onPropertyChanged(
-                sender: androidx.databinding.Observable?,
-                propertyId: Int
+                    sender: androidx.databinding.Observable?,
+                    propertyId: Int
             ) {
                 LogUtils.tag("main").i("我改变了$propertyId")
             }
@@ -118,12 +129,12 @@ class LiveDataTestVM(application: Application) : BaseVM(application) {
 
         override fun onActive() {
             disposable = observable
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    setValue(it)
-                }, {
-                    setValue(null)
-                })
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        setValue(it)
+                    }, {
+                        setValue(null)
+                    })
         }
 
         override fun onInactive() {
@@ -133,6 +144,6 @@ class LiveDataTestVM(application: Application) : BaseVM(application) {
     //endregion
 
     var entries: SingleLiveEvent<List<String>> =
-        SingleLiveEvent(mutableListOf("测试1", "测试2", "测试3", "测试4"))
+            SingleLiveEvent(mutableListOf("测试1", "测试2", "测试3", "测试4"))
     var select: SingleLiveEvent<String> = SingleLiveEvent("测试3")
 }

c7ea2fb0fbf9e642ade52714ea12dab450850960
jiaoyaning <jiaoyaning@igengmei.com>
2021-10-22 10:23:05 +0800

    update

---
+++ app/src/main/java/com/jyn/masterroad/view/viewoverlay/ViewOverlayActivity.kt
@@ -0,0 +1,16 @@
+package com.jyn.masterroad.view.viewoverlay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jyn.masterroad.R
+
+/*
+ * ViewOverlay与ViewGroupOverlay
+ * https://blog.csdn.net/jxq1994/article/details/80158881
+ */
+class ViewOverlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_overlay)
+    }
+}

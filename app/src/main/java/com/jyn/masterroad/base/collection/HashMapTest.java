65006ee3e19dcda656e24af2dd6d82ac14e3ede8
jiaoyaning <jiaoyaning@igengmei.com>
2021-12-28 16:01:10 +0800

    update

--- app/src/main/java/com/jyn/masterroad/base/collection/HashMapTest.java
+++ app/src/main/java/com/jyn/masterroad/base/collection/HashMapTest.java
@@ -20,10 +20,23 @@ import java.util.HashMap;
  */
 public class HashMapTest {
     /*
-     * 1.7 -> 1.8 优化
+     * 1.7 -> 1.8 优化？
      *      1. 链表 —> 红黑树 ：链表长度超过8时改为红黑树，低于6时才变回链表
      *      2. 头插法 -> 尾插法
      *      3. 扩容时重新计算hash -> 只计算是否需要移动到下一个扩容位
+     *
+     *
+     *
+     * HashMap 的 hash() 算法实现(https://blog.csdn.net/qq_38182963/article/details/78940047)
+     *     key的哈希值 ^ key的哈希值右移16位
+     *   这样是为了防止一些极端情况出现，比如 A为 1000010001110001000001111000000，B 为 0111011100111000101000010100000。
+     *   如果数组长度是16，也就是 15 与运算这两个数，会发现结果都是0。这显然不是一个好的hash算法
+     *
+     *
+     * 初始容量为什么是2的n次幂及扩容为什么是2倍的形式？(https://blog.csdn.net/weixin_44273302/article/details/113733422)
+     *     1. 为了保证得到的新的数组索引和老数组索引一致
+     *     2. rehash时的取余操作，hash % length == hash & (length - 1)这个关系只有在length等于二的幂次方时成立，位运算能比%高效得多。
+     *
      */
     HashMap<String, String> hashMap = new HashMap<>();
 

10f5320c7e3ce2a401932ba6372c2e48ab59fe84
jiaoyaning <jiaoyaning@zuoyebang.com>
2022-02-22 13:54:14 +0800

    update

--- app/src/main/java/com/jyn/masterroad/jetpack/navigation/NavigationActivity.kt
+++ app/src/main/java/com/jyn/masterroad/jetpack/navigation/NavigationActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jyn.masterroad.R
 
+/**
+ * Android JetPack系列之——Navigation
+ * https://www.jianshu.com/p/5cd63ecabbf5
+ */
 class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

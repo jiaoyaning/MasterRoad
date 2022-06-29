4c6b99273e3b625552aa4f93288b8fc774d1f5b5
jiaoyaning <jiaoyaning@wps.cn>
2022-06-29 17:52:48 +0800

    feat(ServiceLoader): 发现新大陆

---
+++ language/src/main/java/com/jyn/language/ServiceLoader/ServiceLoaderTest.kt
@@ -0,0 +1,10 @@
+package com.jyn.language.ServiceLoader
+
+/**
+ * https://blog.51cto.com/binghe001/5244938?b=totalstatistic
+ *
+ * Java | 带你理解 ServiceLoader 的原理与设计思想
+ * https://zhuanlan.zhihu.com/p/212850943
+ */
+class ServiceLoaderTest {
+}

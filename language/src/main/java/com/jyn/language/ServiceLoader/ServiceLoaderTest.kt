90dc84b94dcbea66e063cbe6243396b42f63c8d5
jiaoyaning <jiaoyaning@wps.cn>
2022-06-30 19:10:30 +0800

    feat(ServiceLoader): 发现新大陆

--- language/src/main/java/com/jyn/language/ServiceLoader/ServiceLoaderTest.kt
+++ language/src/main/java/com/jyn/language/ServiceLoader/ServiceLoaderTest.kt
@@ -5,6 +5,10 @@ package com.jyn.language.ServiceLoader
  *
  * Java | 带你理解 ServiceLoader 的原理与设计思想
  * https://zhuanlan.zhihu.com/p/212850943
+ *
+ * google AutoService
+ * https://www.jianshu.com/p/14669ded4b8b
+ * https://github.com/google/auto/tree/master/service
  */
 class ServiceLoaderTest {
 }

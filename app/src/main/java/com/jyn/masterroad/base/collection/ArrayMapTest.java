b57e38215e5667f6babc6dac7c74fbacbf397daf
jiaoyaning <jiaoyaning@wps.cn>
2022-07-05 19:37:11 +0800

    feat(task): 代码行统计

--- app/src/main/java/com/jyn/masterroad/base/collection/ArrayMapTest.java
+++ app/src/main/java/com/jyn/masterroad/base/collection/ArrayMapTest.java
@@ -2,11 +2,22 @@ package com.jyn.masterroad.base.collection;
 
 import android.util.ArrayMap;
 
+import com.apkfuns.logutils.LogUtils;
+
 /**
  * https://droidyue.com/blog/2017/02/12/dive-into-arraymap-in-android/
  */
 public class ArrayMapTest {
     public static void main(String[] args) {
-        ArrayMap<String,String> test = new ArrayMap<>();
+        ArrayMap<String, String> test = new ArrayMap<>();
+    }
+
+    public ArrayMapTest() {
+        log();
+    }
+
+
+    private void log() {
+        LogUtils.tag("ArrayMapTest").i("test");
     }
 }
